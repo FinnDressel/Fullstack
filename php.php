5d10d1c38fd241e5673d3c0e7cdad9a7ea376832
FinnDressel <finn.dressel@gmail.com>
2023-02-07 14:01:59 +0100

    Start

---
+++ php.php
@@ -0,0 +1,17 @@
+<hmtl>
+    <body>
+    <form action="php.php" style="border: 1px solid #000000">
+        <div class="signup">
+            <h1>Student inschrijven</h1>
+            <hr>
+            <label for="email"> <b>Email</b> </label>
+            <input type="text" placeholder="Enter Email" name="email" required>
+
+        </div>
+    </form>
+    </body>
+</hmtl>
+
+<?php
+
+
