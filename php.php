dec6512fafa6fcb7c54c1c8455e609bef33bc578
FinnDressel <finn.dressel@gmail.com>
2023-02-08 09:47:32 +0100

    Form inputs

--- php.php
+++ php.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <hmtl>
     <body>
     <form action="php.php" style="border: 1px solid #000000">
@@ -5,13 +9,19 @@
             <h1>Student inschrijven</h1>
             <hr>
             <label for="email"> <b>Email</b> </label>
-            <input type="text" placeholder="Enter Email" name="email" required>
+            <input type="text" placeholder="Enter Email" name="email" required
 
+            <label for="name"> <b>Name</b> </label>
+            <input type="text" placeholder="Enter Name" name="name" required>
+
+            <label for="password"> <b>Password</b> </label>
+            <input type="password" placeholder="Enter Password" name="password" required>
+
+            <div class="Buttons">
+                <button type="button" class="cancelbtn">Cancel</button>
+                <button type="submit" class="signupbtn">Sign Up</button>
+            </div>
         </div>
     </form>
     </body>
 </hmtl>
-
-<?php
-
-
