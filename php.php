9e484c78b5b7465abdfbd8b359a544c0023bf5f5
FinnDressel <finn.dressel@gmail.com>
2023-02-13 14:11:14 +0100

    DB connection

--- php.php
+++ php.php
@@ -1,6 +1,19 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=fullstackdb", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
+
+
 
 <hmtl>
     <link rel="stylesheet" href="style.css">
