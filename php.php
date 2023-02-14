6caa549e47ccb1f28495a5c94a3b9df7762d6a66
FinnDressel <finn.dressel@gmail.com>
2023-02-14 11:13:13 +0100

    Poging tot data versturen

--- php.php
+++ php.php
@@ -8,33 +8,41 @@ $password = "";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=fullstackdb", $username, $password);
 
-    //error check
+    //connectie check
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch (PDOException $e){
     echo "Connection failed: " . $e->getMessage();
 }
 
-$first_name = $_POST['Firstname'];
-$last_name = $_POST['Lastname'];
-$email = $_POST['email'];
+if(isset($_POST['submitButton'])){
 
-$stmt = $pdo->prepare("INSERT INTO Student (first_name, last_name, email) VALUES (:firstname, :lastname, :email)");
+    $email = $_POST['email'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
 
-$stmt->bindParm(':firstname', $first_name);
-$stmt->bindParm(':lastname', $last_name);
-$stmt->bindParm(':email', $email);
-$stmt->execute();
+    $query = "INSERT INTO student (firstame, lastname, email) VALUES (firstname, lastname, email)";
+    $query_run = $conn->prepare($query);
 
-if($stmt->errorCode()!== '00000'){
-    $errorInfo = $stmt->errorInfo();
-} else{
-    echo "succes";
+    $data = [
+        ':firstname' => $firstname,
+        ':lastname' => $lastname,
+        ':email' => $email,
+    ];
+    $query_execute = $query_run->execute($data);
+
+    if($query_execute){
+        $_SESSION['message'] = "Inserted Successfully";
+        header('Location: php.php');
+        exit(0);
+    } else{
+        $_SESSION['message'] = "Failed";
+        header('Location: php.php');
+        exit(0);
+    }
 }
 
 ?>
-
-
 
 <hmtl>
     <link rel="stylesheet" href="style.css">
@@ -54,7 +62,7 @@ if($stmt->errorCode()!== '00000'){
 
             <div class="Buttons">
                 <button type="button" class="cancelbtn">Cancel</button>
-                <button type="submit" class="signupbtn">Sign Up</button>
+                <button type="submit" name="submitButton" class="signupbtn">Sign Up</button>
             </div>
         </div>
     </form>
